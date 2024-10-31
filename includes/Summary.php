74ee167d61fe2250d3700ebaf9ad0361bb00b11c
alistair3149 <alistair31494322@gmail.com>
2024-10-30 23:22:42 -0400

    Add config to disable custom handling
    - Add config to disable custom handling to default details and summary behavior
    - Add MW 1.39+ support

--- includes/Summary.php
+++ includes/Summary.php
@@ -4,18 +4,29 @@ declare( strict_types=1 );
 
 namespace MediaWiki\Extension\Details;
 
+use MediaWiki\MediaWikiServices;
 use PPFrame;
 use Parser;
-use MediaWiki\Parser\Sanitizer;
+/* TODO: Switch to MediaWiki\\Parser\\Sanitizer when Sanitizer fallback is fully dropped */ 
+use Sanitizer;
 
 class Summary {
+
+	/* @var bool */
+	private static $compatibilityMode = true;
+
 	public static function parserHook( ?string $input, array $args, Parser $parser, PPFrame $frame ) {
+		$config = MediaWikiServices::getInstance()->getMainConfig();
+		self::$compatibilityMode =  $config->get( 'DetailsMWCollapsibleCompatibility' );
+
 		// Add tracking category
 		$parser->addTrackingCategory( 'details-category' );
 
 		// Add modules for collapsible handling
-		$parser->getOutput()->addModuleStyles( [ 'ext.details.styles' ] );
-		$parser->getOutput()->addModules( [ 'ext.details' ] );
+		if ( self::$compatibilityMode === true ) {
+			$parser->getOutput()->addModuleStyles( [ 'ext.details.styles' ] );
+			$parser->getOutput()->addModules( [ 'ext.details' ] );
+		}
 
 		// Render content, stripping <p> if it’s the outermost tag
 		$body = trim( $parser->recursiveTagParse( $input, $frame ) );
